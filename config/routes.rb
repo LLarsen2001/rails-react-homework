3d2f96b8e020e35f7c9ec46de6c776329bc274f7
Lance Larsen <lancelarsen2001@gmail.com>
2022-06-07 14:51:49 -0600

    newRoutes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,19 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do 
+    get '/animals', to:'animals#index' # return all dishes
+
+    post '/animals', to:'animals#create' # create one dish
+ 
+    get '/animals/:id', to: 'animals#show' # returns one dish
+   
+    put '/animals/:id', to: 'animals#update' # update one dish
+ 
+    delete '/animals/:id', to: 'animals#destroy' # destroy one dish
+
+  end
+  
+
+
 end
