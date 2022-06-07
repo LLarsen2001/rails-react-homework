72a413c8ce299dc92203a1e0a686295323d3f10d
Matt Hamp <Matthewhamp3@gmail.com>
2022-06-07 14:58:18 -0600

    Animal cotroller

--- app/controllers/api/animals_controller.rb
+++ app/controllers/api/animals_controller.rb
@@ -1,2 +1,43 @@
 class Api::AnimalsController < ApplicationController
+  before_action :set_animal, only: [:show, :update, :destroy]
+
+    def index
+      render json: Animal.all
+    end
+
+    def show
+      render json: @Animal
+    end
+
+    def create 
+      @Animal = Animal.new(animal_params)
+      if @Animal.save
+        render json: @Animal
+      else
+        # todo err
+      end
+    end
+    
+    def update
+      if @animal.update(animal_params)
+        render Json: @animal
+      else
+        # todo err
+      end
+    end
+    def destroy
+      render json: @animal.destroy
+    end
+
+    def animal_params
+      prams.require(:dish).permit(:name, :price, :description)
+    end
+
+    def set_animal
+      @animal = Animal.find(params[:id])
+    end
+
+
+
+
 end
