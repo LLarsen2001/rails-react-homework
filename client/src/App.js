3129cc219ff899391dfefe20c35138d2f9b3bc7e
Matt Hamp <Matthewhamp3@gmail.com>
2022-06-07 16:01:52 -0600

    Animal controller

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [animals, setAnimals] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getAnimals();
+  }, []);
+
+  const getAnimals = async () => {
+    try {
+      let res = await axios.get("/api/animals");
+      // it is not always going to be res.data
+      console.log("res", res);
+      setAnimals(res.data);
+      setLoading(false);
+    } catch (err) {
+      // check
+      alert("error occured");
+      setError(err);
+      setLoading(false);
+    }
+  };
+
+  const renderAnimals = () => {
+    if (loading) {
+      return <p>loading</p>;
+    }
+    if (error) {
+      return <p>{JSON.stringify(error)}</p>;
+    }
+    return animals.map((d) => {
+      return (
+        <div key={d.id} style={{ margin: "20px", border: "1px solid" }}>
+          <h1>Species: {d.name}</h1>
+          <h2>
+            Age:
+            {d.age}
+          </h2>
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Animals</h1>
+      <div>{renderAnimals()}</div>
     </div>
   );
 }
