25deab09dc0a825ee66618e4a8dc1c198b2b3bc1
Matt Hamp <Matthewhamp3@gmail.com>
2022-06-08 15:58:40 -0600

    all done

---
+++ client/src/Animals.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
+import AnimalForm from "./AnimalForm";
+import UpdateForm from "./UpdateAnimalForm";
+
+function Animals() {
+  const [animals, setAnimals] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    getAnimals();
+  }, []);
+
+  const addAnimal = (animal) => {
+    setAnimals([animal, ...animals]);
+  };
+
+  const deleteAnimal = (id) => {
+    let newAnimals = animals.filter((d) => d.id !== id);
+    setAnimals(newAnimals);
+  };
+  const updateAnimal = (animal) => {
+    let updatedAnimal = animals.map((d) => (d.id === animal.id ? animal : d));
+    setAnimals(updatedAnimal);
+  };
+  const getAnimals = async () => {
+    try {
+      let res = await axios.get("/api/animals");
+      // it is not always going to be res.data
+      console.log("res", res);
+      setAnimals(res.data);
+      setLoading(false);
+    } catch (err) {
+      // check
+      alert("error occured");
+      setError(err);
+      setLoading(false);
+    }
+  };
+
+  const renderAnimals = () => {
+    if (loading) {
+      return <p>loading</p>;
+    }
+    if (error) {
+      return <p>{JSON.stringify(error)}</p>;
+    }
+    return animals.map((d) => {
+      return (
+        <div key={d.id} style={{ margin: "20px", border: "1px solid" }}>
+          <h1>Name: {d.name}</h1>
+          <h2>Species: {d.species}</h2>
+          <h2>
+            Age:
+            {d.age}
+          </h2>
+          <button onClick={() => deleteAnimal(d.id)}>delete</button>
+          <UpdateForm {...d} updateAnimal={updateAnimal} />
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div className="Animal comp">
+      <AnimalForm addAnimal={addAnimal} />
+      <h1>Animals</h1>
+      <div>{renderAnimals()}</div>
+    </div>
+  );
+}
+
+export default Animals;
--- client/src/App.js
+++ client/src/App.js
@@ -3,73 +3,19 @@ import axios from "axios";
 import "./App.css";
 import AnimalForm from "./AnimalForm";
 import UpdateForm from "./UpdateAnimalForm";
+import { Route, Routes } from "react-router-dom";
+import Animals from "./Animals";
+import PageWrapper from "./PageWrapper";
+import Home from "./Home";
 
 function App() {
-  const [animals, setAnimals] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    getAnimals();
-  }, []);
-
-  const addAnimal = (animal) => {
-    setAnimals([animal, ...animals]);
-  };
-
-  const deleteAnimal = (id) => {
-    let newAnimals = animals.filter((d) => d.id !== id);
-    setAnimals(newAnimals);
-  };
-  const updateAnimal = (animal) => {
-    let updatedAnimal = animals.map((d) => (d.id === animal.id ? animal : d));
-    setAnimals(updatedAnimal);
-  };
-  const getAnimals = async () => {
-    try {
-      let res = await axios.get("/api/animals");
-      // it is not always going to be res.data
-      console.log("res", res);
-      setAnimals(res.data);
-      setLoading(false);
-    } catch (err) {
-      // check
-      alert("error occured");
-      setError(err);
-      setLoading(false);
-    }
-  };
-
-  const renderAnimals = () => {
-    if (loading) {
-      return <p>loading</p>;
-    }
-    if (error) {
-      return <p>{JSON.stringify(error)}</p>;
-    }
-    return animals.map((d) => {
-      return (
-        <div key={d.id} style={{ margin: "20px", border: "1px solid" }}>
-          <h1>Name: {d.name}</h1>
-          <h2>Species: {d.species}</h2>
-          <h2>
-            Age:
-            {d.age}
-          </h2>
-          <button onClick={() => deleteAnimal(d.id)}>delete</button>
-          <UpdateForm {...d} updateAnimal={updateAnimal} />
-        </div>
-      );
-    });
-  };
-
   return (
-    <div className="App">
-      <AnimalForm addAnimal={addAnimal} />
-
-      <h1>Animals</h1>
-      <div>{renderAnimals()}</div>
-    </div>
+    <Routes>
+      <Route element={<PageWrapper />}>
+        <Route index element={<Home />} />
+        <Route path="/animals" element={<Animals />} />
+      </Route>
+    </Routes>
   );
 }
 
